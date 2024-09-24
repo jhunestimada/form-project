49d1b79e3200cbc5f183df4db43376e58ff75817
Jhun Estimada <jhunestimada@gmail.com>
2024-09-24 17:15:17 +0800

    Coding Challenge #1 - DOM, added

---
+++ Js/Coding Challenge #1 - DOM/script.js
@@ -0,0 +1,85 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+
+function myFunction1()
+{
+        var x = document.getElementById("specification");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+          } else {
+            x.style.display = "none";
+          }
+
+          var y = document.getElementById("registerProduct");
+        if (y.style.display === "none") {
+            y.style.display = "block";
+          } else {
+            y.style.display = "none";
+          }
+}   
+
+function myFunction2()
+{
+        var x = document.getElementById("whoLenovo");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+          } else {
+            x.style.display = "none";
+          }
+
+          var y = document.getElementById("registerProduct");
+        if (y.style.display === "none") {
+            y.style.display = "block";
+          } else {
+            y.style.display = "none";
+          }
+}   
+
+
+function myFunction3()
+{
+        var x = document.getElementById("whoLenovo");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+          } else {
+            x.style.display = "none";
+          }
+
+          var y = document.getElementById("specification");
+        if (y.style.display === "none") {
+            y.style.display = "block";
+          } else {
+            y.style.display = "none";
+          }
+
+          
+}  
+
+function myFunction4()
+{
+        var x = document.getElementById("whoLenovo");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+          } else {
+            x.style.display = "none";
+          }
+
+          var y = document.getElementById("specification");
+        if (y.style.display === "none") {
+            y.style.display = "block";
+          } else {
+            y.style.display = "none";
+          }
+
+          var z = document.getElementById("registerProduct");
+          if (z.style.display === "none") {
+              z.style.display = "block";
+            } else {
+              z.style.display = "none";
+            }
+}  
+   
+     
